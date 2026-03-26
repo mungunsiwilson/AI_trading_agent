bbf84286d721a0e67924f957afc37d0db5fa7068
Mungunsi Wilson <98978190+mungunsiwilson@users.noreply.github.com>
2026-03-26 10:17:24 +0800

    Delete douyin_opportunity_finder directory

--- douyin_opportunity_finder/email_services/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Email services package."""
-
-from .email_sender import EmailService, create_email_service_from_env, parse_email_list
-
-__all__ = ['EmailService', 'create_email_service_from_env', 'parse_email_list']
